11868a9eb6da5a7089b7a6c7b4cfa691cc8d04ef
jeonghopark <jeonghopark@jeonghopark.de>
2017-01-06 18:21:34 +0100

    First rotation set Circle

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 30;
+// camera.rotation.x = 0.4;
+camera.position.y = 0;
+camera.position.z = 20;
+camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -9,9 +12,14 @@ document.body.appendChild(renderer.domElement);
 var geom = new THREE.Geometry();
 
 for (var i = 0; i < 20; i++) {
-    var v1 = new THREE.Vector3(i, 0, 0);
-    var v2 = new THREE.Vector3(i + 1, 1, 0);
-    var v3 = new THREE.Vector3(i, 1, 0);
+    var _size = 10;
+    var _x1 = Math.cos(THREE.Math.degToRad(i*360/20)) * _size;
+    var _x2 = Math.cos(THREE.Math.degToRad((i+1)*360/20)) * _size;
+    var _z1 = Math.sin(THREE.Math.degToRad((i)*360/20)) * _size;
+    var _z2 = Math.sin(THREE.Math.degToRad((i+1)*360/20)) * _size;
+    var v1 = new THREE.Vector3(_x1, 0, _z1);
+    var v2 = new THREE.Vector3(_x2, 1, _z2);
+    var v3 = new THREE.Vector3(_x1, 1, _z1);
     geom.vertices.push(v1);
     geom.vertices.push(v2);
     geom.vertices.push(v3);
@@ -23,6 +31,7 @@ for (var i = 0; i < geom.vertices.length - 3; i += 3) {
     geom.computeFaceNormals();
 }
 
+
 var object = new THREE.Mesh(geom, new THREE.MeshNormalMaterial());
 var wireMat = new THREE.MeshBasicMaterial({
     color: "rgb(0,255,0)",
@@ -30,10 +39,14 @@ var wireMat = new THREE.MeshBasicMaterial({
     transparent: true,
     overdraw: true
 });
-scene.add(object);
 
 var wireMesh = new THREE.Mesh(geom, wireMat);
+
+object.rotation.x = 0.5;
+wireMesh.rotation.x = 0.5;
+
 scene.add(wireMesh);
+scene.add(object);
 
 function render() {
     requestAnimationFrame(render);
