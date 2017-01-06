be40b29ee44f61d1ebd88b25459ac5066639dce3
jeonghopark <jeonghopark@jeonghopark.de>
2017-01-06 20:12:47 +0100

    Rotate Change Color

--- script.js
+++ script.js
@@ -1,55 +1,88 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-// camera.rotation.x = 0.4;
-camera.position.y = 0;
-camera.position.z = 20;
-camera.lookAt(new THREE.Vector3(0, 0, 0));
+var clock;
+var container;
+var scene; 
+var stats;
+var camera;
+var wireMesh;
+var object;
+var renderer;
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+init();
+animate();
 
-var geom = new THREE.Geometry();
+function init() {
+    
+    clock = new THREE.Clock();
 
-for (var i = 0; i < 20; i++) {
-    var _size = 10;
-    var _x1 = Math.cos(THREE.Math.degToRad(i*360/20)) * _size;
-    var _x2 = Math.cos(THREE.Math.degToRad((i+1)*360/20)) * _size;
-    var _z1 = Math.sin(THREE.Math.degToRad((i)*360/20)) * _size;
-    var _z2 = Math.sin(THREE.Math.degToRad((i+1)*360/20)) * _size;
-    var v1 = new THREE.Vector3(_x1, 0, _z1);
-    var v2 = new THREE.Vector3(_x2, 1, _z2);
-    var v3 = new THREE.Vector3(_x1, 1, _z1);
-    geom.vertices.push(v1);
-    geom.vertices.push(v2);
-    geom.vertices.push(v3);
+    container = document.getElementById('container');
+
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.y = 0;
+    camera.position.z = 20;
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.domElement.style.position = "relative";
+    renderer.gammaInput = true;
+    renderer.gammaOutput = true;
+    renderer.shadowMap.enabled = true;
+
+    container.appendChild(renderer.domElement);
+
+    stats = new Stats();
+    container.appendChild(stats.dom);
+
+    var _geom = new THREE.Geometry();
+    var _step = 20;
+    for (var i = 0; i < _step; i++) {
+        var _size = 10;
+        var _x1 = Math.cos(THREE.Math.degToRad(i * 360 / _step)) * _size;
+        var _x2 = Math.cos(THREE.Math.degToRad((i + 1) * 360 / _step)) * _size;
+        var _z1 = Math.sin(THREE.Math.degToRad((i) * 360 / _step)) * _size;
+        var _z2 = Math.sin(THREE.Math.degToRad((i + 1) * 360 / _step)) * _size;
+        var v1 = new THREE.Vector3(_x1, 0, _z1);
+        var v2 = new THREE.Vector3(_x2, 1, _z2);
+        var v3 = new THREE.Vector3(_x1, 1, _z1);
+        _geom.vertices.push(v1);
+        _geom.vertices.push(v2);
+        _geom.vertices.push(v3);
+    }
+    for (var i = 0; i < _geom.vertices.length - 3; i += 3) {
+        _geom.faces.push(new THREE.Face3(i + 1, i, i + 2));
+        _geom.faces.push(new THREE.Face3(i + 3, i, i + 1));
+        _geom.computeFaceNormals();
+    }
+
+    var _norMat = new THREE.MeshBasicMaterial({color: "rgb(255, 255, 255)"});
+
+    object = new THREE.Mesh(_geom, _norMat);
+
+    var _wireMat = new THREE.MeshBasicMaterial({
+        color: "rgb(0,255,0)",
+        wireframe: true,
+        transparent: true,
+        overdraw: true
+    });
+
+    wireMesh = new THREE.Mesh(_geom, _wireMat);
+
+    scene.add(wireMesh);
+    scene.add(object);
 }
 
-for (var i = 0; i < geom.vertices.length - 3; i += 3) {
-    geom.faces.push(new THREE.Face3(i, i + 1, i + 2));
-    geom.faces.push(new THREE.Face3(i, i + 3, i + 1));
-    geom.computeFaceNormals();
+
+function animate() {
+    requestAnimationFrame(animate);
+    render();
+    stats.update();
 }
-
-
-var object = new THREE.Mesh(geom, new THREE.MeshNormalMaterial());
-var wireMat = new THREE.MeshBasicMaterial({
-    color: "rgb(0,255,0)",
-    wireframe: true,
-    transparent: true,
-    overdraw: true
-});
-
-var wireMesh = new THREE.Mesh(geom, wireMat);
-
-object.rotation.x = 0.5;
-wireMesh.rotation.x = 0.5;
-
-scene.add(wireMesh);
-scene.add(object);
 
 function render() {
-    requestAnimationFrame(render);
+    var _delta = clock.getElapsedTime() * 0.5;
+    object.rotation.x = _delta;
+    wireMesh.rotation.x = _delta;
     renderer.render(scene, camera);
 }
-render();
