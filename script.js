24eec4538a989a33e303af3597f67df953df57ba
jeonghopark <jeonghopark@jeonghopark.de>
2017-01-06 11:51:55 +0100

    check for statement

--- script.js
+++ script.js
@@ -6,22 +6,32 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 var geom = new THREE.Geometry();
-var v1 = new THREE.Vector3(0, 0, 0);
-var v2 = new THREE.Vector3(1, 1, 0);
-var v3 = new THREE.Vector3(0, 1, 0);
 
-geom.vertices.push(v1);
-geom.vertices.push(v2);
-geom.vertices.push(v3);
+for (var i = 0; i < 20; i++) {
+        var v1 = new THREE.Vector3(i, i, 0);
+        var v2 = new THREE.Vector3(i + 1, i + 1, 0);
+        var v3 = new THREE.Vector3(i, i + 1, 0);
+        geom.vertices.push(v1);
+        geom.vertices.push(v2);
+        geom.vertices.push(v3);
+}
 
-geom.faces.push(new THREE.Face3(0, 1, 2));
-geom.computeFaceNormals();
+// var v1 = new THREE.Vector3(0, 0, 0);
+// var v2 = new THREE.Vector3(1, 1, 0);
+// var v3 = new THREE.Vector3(0, 1, 0);
+
+for (var i = 0; i < geom.vertices.length-2; i++) {    
+    geom.faces.push(new THREE.Face3(i, i + 1, i + 2));
+    geom.computeFaceNormals();
+}
+
+// geom.faces.push(new THREE.Face3(0, 1, 2));
 
 var object = new THREE.Mesh(geom, new THREE.MeshNormalMaterial());
 
 scene.add(object);
 
-camera.position.z = 10;
+camera.position.z = 30;
 
 function render() {
     requestAnimationFrame(render);
