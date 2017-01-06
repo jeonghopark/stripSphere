9bf7b04c85376e4d839925dea57397fab259c86b
jeonghopark <jeonghopark@jeonghopark.de>
2017-01-06 13:09:22 +0100

    cleanup code

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 30;
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -8,34 +9,31 @@ document.body.appendChild(renderer.domElement);
 var geom = new THREE.Geometry();
 
 for (var i = 0; i < 20; i++) {
-        var v1 = new THREE.Vector3(i, 0, 0);
-        var v2 = new THREE.Vector3(i + 1, 1, 0);
-        var v3 = new THREE.Vector3(i, 1, 0);
-        geom.vertices.push(v1);
-        geom.vertices.push(v2);
-        geom.vertices.push(v3);
+    var v1 = new THREE.Vector3(i, 0, 0);
+    var v2 = new THREE.Vector3(i + 1, 1, 0);
+    var v3 = new THREE.Vector3(i, 1, 0);
+    geom.vertices.push(v1);
+    geom.vertices.push(v2);
+    geom.vertices.push(v3);
 }
 
-// var v1 = new THREE.Vector3(0, 0, 0);
-// var v2 = new THREE.Vector3(1, 1, 0);
-// var v3 = new THREE.Vector3(0, 1, 0);
-
-for (var i = 0; i < geom.vertices.length-2; i+=3) {    
+for (var i = 0; i < geom.vertices.length - 3; i += 3) {
     geom.faces.push(new THREE.Face3(i, i + 1, i + 2));
+    geom.faces.push(new THREE.Face3(i, i + 3, i + 1));
     geom.computeFaceNormals();
 }
 
-console.log(geom.vertices.length);
-
-// geom.faces.push(new THREE.Face3(0, 1, 2));
-// geom.faces.push(new THREE.Face3(3, 4, 5));
-// geom.computeFaceNormals();
-
 var object = new THREE.Mesh(geom, new THREE.MeshNormalMaterial());
-
+var wireMat = new THREE.MeshBasicMaterial({
+    color: "rgb(0,255,0)",
+    wireframe: true,
+    transparent: true,
+    overdraw: true
+});
 scene.add(object);
 
-camera.position.z = 30;
+var wireMesh = new THREE.Mesh(geom, wireMat);
+scene.add(wireMesh);
 
 function render() {
     requestAnimationFrame(render);
