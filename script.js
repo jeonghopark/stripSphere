2d04018b9d17eab6a2e74bff6eee31f5ab21645e
jeonghopark <jeonghopark@jeonghopark.de>
2017-01-07 22:55:15 +0100

    cleanup Background gradient

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var wireMeshS = new Array();
 var object;
 var objectS = new Array();;
 
-var gNum = 20;
+var gNum = 40;
 
 init();
 animate();
@@ -16,25 +16,29 @@ animate();
 
 function init() {
     
+    if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
+
     clock = new THREE.Clock();
 
     container = document.getElementById('container');
 
     scene = new THREE.Scene();
+    // scene.background = new THREE.Color( 0xFFEAB0 );
+    // scene.fog = new THREE.FogExp2( 0xcccccc, 0.01 ); //https://threejs.org/examples/#misc_controls_trackball
 
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.y = 0;
-    camera.position.z = 20;
+    camera.position.z = 25;
     camera.lookAt(new THREE.Vector3(0, 0, 0));
 
     renderer = new THREE.WebGLRenderer({
-        antialias: true
+        antialias: true,
+        alpha: true
     });
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.domElement.style.position = "relative";
-    renderer.gammaInput = true;
-    renderer.gammaOutput = true;
-    renderer.shadowMap.enabled = true;
+    // renderer.gammaInput = true;
+    // renderer.gammaOutput = true;
+    // renderer.setClearColor( 0x00ff00, 0);
 
     container.appendChild(renderer.domElement);
 
@@ -47,12 +51,29 @@ function init() {
     light.position.z = 15;
     light.intensity = 1;
 
-    controls = new THREE.OrbitControls( camera, renderer.domElement );
+    lightIn = new THREE.PointLight(0xFEF8D1, 1, 10 );
+    lightIn.position.x = 0;
+    lightIn.position.y = 0;
+    lightIn.position.z = 0;
+    lightIn.intensity = 1;
 
+
+    controls = new THREE.TrackballControls( camera );
+    controls.rotateSpeed = 3.0;
+    controls.zoomSpeed = 1.2;
+    controls.panSpeed = 0.8;
+    controls.noZoom = false;
+    controls.noPan = false;
+    controls.staticMoving = true;
+    controls.dynamicDampingFactor = 0.3;
+    // controls.keys = [ 65, 83, 68 ];
+    controls.addEventListener( 'change', render );
+    
+        
     var _norMat = new THREE.MeshLambertMaterial({
         color: 0xffffff, 
         shading: THREE.SmoothShading,
-        side: THREE.DoubleSide
+        side: THREE.FrontSide
     });
 
 
@@ -60,6 +81,8 @@ function init() {
     for (var i = 0; i < gNum; i++) {
         _norMatS[i] = new THREE.MeshLambertMaterial({
             color: 0xffffff, 
+            opacity: 0.75,
+            transparent: true,
             shading: THREE.SmoothShading,
             side: THREE.DoubleSide
         });
@@ -67,7 +90,7 @@ function init() {
 
     var _geomS = new Array();
     for (var i = 0; i < gNum; i++) {
-        _geomS[i] = geom(36, 6 + 0.1 * i);
+        _geomS[i] = geom(36, 6 + 0.12 * i);
         objectS[i] = new THREE.Mesh(_geomS[i], _norMatS[i]);
     }
 
@@ -83,12 +106,14 @@ function init() {
         wireMeshS[i] = new THREE.Mesh(_geomS[i], _wireMat);
     }
 
-    scene.add(new THREE.AmbientLight(0x050505));
+    scene.add(new THREE.AmbientLight(0x330505));
     for (var i = 0; i < gNum; i++) {
         scene.add(objectS[i]);
-        scene.add(wireMeshS[i]);
+        // scene.add(wireMeshS[i]);
     }
+
     scene.add(light);
+    scene.add(lightIn);
 
 }
 
@@ -97,7 +122,7 @@ function geom(_step, _size){
     this._geom = new THREE.Geometry();
     this._step = _step;
     this._size = _size;
-    this._ySize = 1.5;
+    this._ySize = 1.0;
     for (var i=0; i<=_step; i+=1) {
         var _x1 = Math.cos(THREE.Math.degToRad(i * 360 / _step)) * _size;
         var _x2 = Math.cos(THREE.Math.degToRad((i + 1) * 360 / _step)) * _size;
@@ -122,6 +147,7 @@ function geom(_step, _size){
 function animate() {
     requestAnimationFrame(animate);
     render();
+    controls.update();
     stats.update();
 }
 
@@ -129,8 +155,10 @@ function animate() {
 function render() {
     var _delta = clock.getElapsedTime() * 0.8;
     for (var i = 0; i < gNum; i++) {
-        objectS[i].rotation.x = _delta * i * 0.1;
-        wireMeshS[i].rotation.x = _delta * i * 0.1;
+        objectS[i].rotation.y = Math.PI;
+        objectS[i].rotation.z = Math.PI * 0.25;
+        objectS[i].rotation.x = _delta * (i + 5) * 0.1;
+        wireMeshS[i].rotation.x = _delta * (i + 5) * 0.1;
     }
     renderer.render(scene, camera);
 }
