9ffa59ba1088d3861109a0d63bde6049da801105
jeonghopark <jeonghopark@jeonghopark.de>
2017-01-11 11:06:23 +0100

    More functions
    guisetting

--- script.js
+++ script.js
@@ -17,14 +17,19 @@ function init() {
 
     if (!Detector.webgl) Detector.addGetWebGLMessage();
 
+    parameters = {
+        StripWidth: 1.0,
+        Speed: 0.5,
+        RotationY: 45,
+        RotationZ: 25
+    }
+
     clock = new THREE.Clock();
     clock.start();
 
-
     scene = new THREE.Scene();
     // scene.background = new THREE.Color( 0xFFEAB0 );
-    // scene.fog = new THREE.FogExp2( 0xcccccc, 0.01 ); //https://threejs.org/examples/#misc_controls_trackball
-    scene.add(new THREE.AmbientLight(0x330505));
+    // scene.fog = new THREE.FogExp2( 0xcccccc, 0.01 ); 
 
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 25;
@@ -46,9 +51,28 @@ function init() {
     document.body.appendChild(renderer.domElement);
     container.appendChild(stats.dom);
 
-    lightSetting();
+    lightSetting(scene);
+    controlsSetting(renderer);
 
-    controlsSetting();
+    geoMeshSetting();
+
+    guiSetting();
+
+
+}
+
+
+
+//-----------------------------------------------------------------------------
+function geoMeshSetting() {
+
+    var _wireMatE = new THREE.MeshBasicMaterial({
+        color: 0x00ff00,
+        opacity: 0.3,
+        wireframe: true,
+        transparent: true,
+        overdraw: true
+    });
 
     var _norMatS = new Array();
     for (var i = 0; i < gNum; i += 1) {
@@ -63,57 +87,24 @@ function init() {
 
     var _geomS = new Array();
     for (var i = 0; i < gNum; i += 1) {
-        _geomS[i] = geom(36, 6 + 0.12 * i, 1.0);
+        scene.remove(objectS[i]);
+        _geomS[i] = geom(36, 6 + 0.12 * i);
         objectS[i] = new THREE.Mesh(_geomS[i], _norMatS[i]);
-    }
-
-    var _wireMatE = new THREE.MeshBasicMaterial({
-        color: 0x00ff00,
-        opacity: 0.3,
-        wireframe: true,
-        transparent: true,
-        overdraw: true
-    });
-    for (var i = 0; i < gNum; i += 1) {
         wireMeshS[i] = new THREE.Mesh(_geomS[i], _wireMatE);
-    }
-
-    for (var i = 0; i < gNum; i += 1) {
         scene.add(objectS[i]);
         // scene.add(wireMeshS[i]);
     }
 
-    gui = new dat.GUI({
-        height: 0
-    });
-    parameters = {
-        StripWidth: 1.0,
-        Speed: 0.5,
-        RotationY: 45,
-        RotationZ: 25
-    }
-    var stripWidth = gui.add(parameters, 'StripWidth').min(0.2).max(3.0).step(0.1).listen();
-    gui.add(parameters, 'Speed').min(0.0).max(1.0).step(0.1).listen();
-    stripWidth.onChange(function() {
-        var _geomS = new Array();
-        for (var i = 0; i < gNum; i += 1) {
-            scene.remove(objectS[i]);
-            _geomS[i] = geom(36, 6 + 0.12 * i, parameters.StripWidth);
-            objectS[i] = new THREE.Mesh(_geomS[i], _norMatS[i]);
-            scene.add(objectS[i]);
-        }
-    });
-    
-
 }
 
 
+
 //-----------------------------------------------------------------------------
-function geom(_step, _size, stripWidth) {
+function geom(_step, _size) {
     this._geom = new THREE.Geometry();
     this._step = _step;
     this._size = _size;
-    this._ySize = stripWidth;
+    this._ySize = parameters.StripWidth;
     for (var i = 0; i <= _step; i += 1) {
         var _x1 = Math.cos(THREE.Math.degToRad(i * 360 / _step)) * _size;
         var _x2 = Math.cos(THREE.Math.degToRad((i + 1) * 360 / _step)) * _size;
@@ -144,8 +135,10 @@ function animate() {
 }
 
 
+
 //-----------------------------------------------------------------------------
 function render() {
+
     var _delta = Math.PI * counter() * 0.05 / 180.0;
     var _followIndex = 20;
     for (var i = 0; i < gNum; i += 1) {
@@ -156,6 +149,7 @@ function render() {
         wireMeshS[i].rotation.z = Math.PI * 0.25;
         wireMeshS[i].rotation.x = _delta * (i + _followIndex);
     }
+
     renderer.render(scene, camera);
 }
 
@@ -164,13 +158,18 @@ function render() {
 function counter() {
     if (clock.running) {
         counter.count = counter.count + (1.0 * parameters.Speed) || 1;
+    } else {
+        counter.count = 1;
     }
     return counter.count;
 }
 
 
+
+
+
 //-----------------------------------------------------------------------------
-function lightSetting(){
+function lightSetting(_scene) {
     light = new THREE.PointLight(0xffffff, 1, 50);
     light.position.x = 0;
     light.position.y = 2;
@@ -181,16 +180,17 @@ function lightSetting(){
     lightIn.position.x = 0;
     lightIn.position.y = 0;
     lightIn.position.z = 0;
-    lightIn.intensity = 1;    
-    
-    scene.add(light);
-    scene.add(lightIn);
+    lightIn.intensity = 1;
+
+    _scene.add(light);
+    _scene.add(lightIn);
+    _scene.add(new THREE.AmbientLight(0x333355));
 }
 
 
 //-----------------------------------------------------------------------------
-function controlsSetting(){
-    controls = new THREE.TrackballControls(camera, renderer.domElement);
+function controlsSetting(_renderer) {
+    controls = new THREE.TrackballControls(camera, _renderer.domElement);
     controls.rotateSpeed = 3.0;
     controls.zoomSpeed = 1.2;
     controls.panSpeed = 0.8;
@@ -198,4 +198,17 @@ function controlsSetting(){
     controls.noPan = false;
     controls.staticMoving = true;
     controls.dynamicDampingFactor = 0.3;
+}
+
+
+//-----------------------------------------------------------------------------
+function guiSetting() {
+    gui = new dat.GUI();
+
+    gui.add(parameters, 'Speed').min(0.0).max(1.0).step(0.1).listen();
+
+    var _stripWidth = gui.add(parameters, 'StripWidth').min(0.2).max(3.0).step(0.1).listen();
+    _stripWidth.onChange(function() {
+        geoMeshSetting();
+    });
 }
